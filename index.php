a37806a927ee960894e50e0a73b6f4c23b0c1603
Chris Haas <chris@vendiadvertising.com>
2018-09-26 13:52:55 -0500

    Perform sanity checks for WordPress and template router

--- index.php
+++ index.php
@@ -1,8 +1,18 @@
 <?php
-$user = wp_get_current_user();
-if( 0 == $user->ID )
-{
-    auth_redirect();
+
+if(!\defined('ABSPATH')){
+    \header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
+    die('Direct file access not allowed');
 }
 
-wp_safe_redirect( \Vendi\Shared\template_router::get_instance()->create_url() );
+if(!\class_exists('Vendi\Shared\template_router')){
+    \header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
+    die('This theme requires a controlling plugin with \Vendi\Shared\template_router installed.');
+}
+
+$user = \wp_get_current_user();
+if( 0 == $user->ID ){
+    \auth_redirect();
+}
+
+\wp_safe_redirect( \Vendi\Shared\template_router::get_instance()->create_url() );
